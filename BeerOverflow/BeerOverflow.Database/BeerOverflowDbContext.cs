c07d341986c5437da47aead2736fda0b42a36976
Iliyan Syarov <isyarov98@gmail.com>
2020-10-07 19:33:14 +0300

    Class BeerOverflowDbContext was created.

---
+++ BeerOverflow/BeerOverflow.Database/BeerOverflowDbContext.cs
@@ -0,0 +1,30 @@
+﻿using BeerOverflow.Database.Configuration;
+using BeerOverflow.Models.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerOverflow.Database
+{
+    public class BeerOverflowDbContext : IdentityDbContext<User, Role, int>
+    {
+        public BeerOverflowDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Beer> Beers { get; set; }
+        public DbSet<Brewery> Breweries { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Style> Styles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BeerConfig());
+            modelBuilder.ApplyConfiguration(new BreweryConfig());
+            modelBuilder.ApplyConfiguration(new ReviewConfig());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
